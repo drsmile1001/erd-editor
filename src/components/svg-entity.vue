fc55a52c04186424ef3737d1aa188c3843e2f9a5
drsmile <drsmile.1001@gmail.com>
2018-08-18 15:06:29 +0800

    svg畫布和svg內嵌table

---
+++ src/components/svg-entity.vue
@@ -0,0 +1,60 @@
+<template>
+    <foreignObject :x="xx"
+                   :y="yy"
+                   :width="width"
+                   :height="height"
+                   @click="click">
+        <table :focus="selected">
+            <thead>
+                <tr>
+                    <th>欄位</th>
+                    <th>型別</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item,idx) in items"
+                    :key="idx">
+                    <td>{{item.field}}</td>
+                    <td>{{item.type}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </foreignObject>
+</template>
+
+<script>
+export default {
+    props: {
+        x: Number,
+        y: Number,
+        width: Number,
+        height: Number,
+        items: Array,
+        selected:Boolean
+    },
+    computed: {
+        xx() {
+            return this.x - this.width / 2
+        },
+        yy() {
+            return this.y - this.height / 2
+        }
+    },
+    methods:{
+        click(){
+            this.$emit("click");
+        }
+    }
+}
+</script>
+
+<style scoped>
+table{
+    width:100%;
+    height: 100%;
+    background-color: pink;
+}
+table[focus]{
+    background-color: red;
+}
+</style>
---
+++ src/components/svg-rect.vue
@@ -0,0 +1,32 @@
+<template>
+    <g :transform="transform">
+        <rect fill="#FF6CC4"
+              stroke="#C30D23"
+              stroke-width="3"
+              width="75"
+              height="75">
+            
+        </rect>
+        <text font-size="14"
+              text-anchor="middle">
+            <tspan>Employee</tspan>
+        </text>
+    </g>
+</template>
+
+<script>
+export default {
+    props: {
+        x: Number,
+        y: Number
+    },
+    computed:{
+        transform(){
+            return `translate(${this.x},${this.y})`
+        }
+    }
+}
+</script>
+
+<style>
+</style>
